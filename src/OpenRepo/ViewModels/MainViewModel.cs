59fc2c0016fb92a3c4e2ee4ad21e5b85522536a6
Erik Kvanli <0erikkvanli0@gmail.com>
2019-10-15 10:52:50 +0200

    Key to open next level actions

--- src/OpenRepo/ViewModels/MainViewModel.cs
+++ src/OpenRepo/ViewModels/MainViewModel.cs
@@ -58,7 +58,7 @@ namespace OpenRepo.ViewModels
             {
                 var tempTraverser = new IndexTraverser(m_traverser.Current, m_currentItems.Count);
                 var visibleItems = new List<SelectableItem>();
-                var max = m_currentItems.Count > 4 ? 11 : m_currentItems.Count;
+                var max = m_currentItems.Count > 11 ? 11 : m_currentItems.Count;
                 var selected = max / 3;
                 for (var i = 0; i < selected; i++) tempTraverser.MovePrevious();
                 for(var i = 0; i < max; i++)
