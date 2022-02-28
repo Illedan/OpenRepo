ee3bf05cec470d4506dcec15bfbb6f76a0254a49
Erik Kvanli <0erikkvanli0@gmail.com>
2022-02-28 22:52:12 +0100

    Solves #36

---
+++ src/OpenRepo/Services/CopyToClipboardService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextCopy;
+
+namespace Illedan.OpenRepo.Services
+{
+    public static class CopyToClipboardService
+    {
+        public static void AddTextToClipboard(string text)
+        {
+            ClipboardService.SetText(text);
+        }
+    }
+}
